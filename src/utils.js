7d816987b7642bfe7e38a15813b3735e918fc361
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-07-05 15:35:08 +0200

    Added utils method to sort an array of objects by column and sort order

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const orderBy = require('lodash/orderBy');
+
 /**
  * Encode a string as a URI component
  * @param {String} query to encode
@@ -5,4 +7,15 @@
  */
 const sanitizeQuery = query => encodeURIComponent(query);
 
+/**
+ * Get a sorted array of objects sorted by object key value and by asc or desc order
+ * @param {Array} items
+ * @param {String} column
+ * @param {String} order asc or desc
+ */
+const sortByColumn = ({ items, column, order }) => {
+  return orderBy(items, [column], [order]);
+};
+
 exports.sanitizeQuery = sanitizeQuery;
+exports.sortByColumn = sortByColumn;
