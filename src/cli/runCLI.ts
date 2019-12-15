d17666977dd132c5690c359e7ed7ef0e3b9b7c58
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-12-15 16:44:53 +0100

    refactor creation of cli and user input flow

--- src/cli/runCLI.ts
+++ src/cli/runCLI.ts
@@ -3,27 +3,38 @@ import * as inquirer from 'inquirer';
 import { IMDb } from './IMDb';
 import { inquirerPromptQuestion } from './settings/inquirerPromptQuestion';
 
+/**
+ * Run the CLI program by creating a instance of IMDb CLI
+ * and run methods from it based of user input
+ * @param cliProgram program.CommanderStatic
+ */
 export const runCLI = async (cliProgram: program.CommanderStatic) => {
-  const { plot, limitPlot, orderBy, movies, series, title, info } = cliProgram;
-  const imdbInstance = new IMDb({
+  const imdbInstance = createCLI(cliProgram);
+
+  if (hasUserInput(cliProgram, 'title') && hasUserInput(cliProgram, 'info')) {
+    imdbInstance.seriesInfo();
+    return;
+  }
+  if (hasUserInput(cliProgram, 'title')) {
+    imdbInstance.search();
+    return;
+  }
+  // prompt the user for a search string
+  const { searchString } = await inquirer.prompt(inquirerPromptQuestion);
+  imdbInstance.searchQuery = searchString;
+  imdbInstance.search();
+};
+
+const createCLI = (cliProgram: program.CommanderStatic) => {
+  const { plot, limitPlot, orderBy, movies, series, title } = cliProgram;
+  const CLI = new IMDb({
+    query: title,
     showPlot: !!plot,
     limitPlot,
     sortColumn: orderBy,
     searchByType: IMDb.determineType({ movies, series }),
   });
-  if (hasUserInput(cliProgram, 'title')) {
-    imdbInstance.searchQuery = title;
-    if (hasUserInput(cliProgram, 'info')) {
-      imdbInstance.seriesInfo();
-    } else {
-      imdbInstance.search();
-    }
-  } else {
-    // prompt the user for a search string
-    const { searchString } = await inquirer.prompt(inquirerPromptQuestion);
-    imdbInstance.searchQuery = searchString;
-    imdbInstance.search();
-  }
+  return CLI;
 };
 
 const hasUserInput = (cliProgram: program.CommanderStatic, input: string) =>  !!cliProgram[input];
