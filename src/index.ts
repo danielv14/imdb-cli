6ae8892146b36df8c8f8688464c76f29ec4ac07e
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-12-15 21:16:44 +0100

    import dotenv over require

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import path from 'path';
-// set path of dotenv to make the CLI gather .env file when called from outside project root folder
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 import clear from 'clear';
 import program from 'commander';
+import dotenv from 'dotenv';
+import path from 'path';
 import { processProgramArgs } from './cli/programArgs';
 import { CLI_COLOR, renderCLIHeader } from './cli/renderer/renderer';
 import { runCLI } from './cli/runCLI';
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 processProgramArgs(program);
 
