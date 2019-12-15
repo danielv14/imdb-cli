951dfa8000524d81c28b5ac0bd93a716207c805b
Daniel Vernberg <daniel.vernberg@gmail.com>
2019-12-15 19:08:56 +0100

    update axios import

--- src/lib/omdbApi.ts
+++ src/lib/omdbApi.ts
@@ -1,4 +1,4 @@
-const axios = require('axios');
+import axios from 'axios';
 import { FullItem, Item, SearchResultType } from '../types/searchResult';
 import { Season, Series } from '../types/series';
 import { sanitizeQuery } from './utils';
